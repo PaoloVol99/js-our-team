ee7b8a8602c0ff8e86ad799a6b7dd52839eab3fa
“PaoloVol99” <paolo.volpini99@gmail.com>
2023-02-14 15:37:54 +0100

    FEAT array created (milestone 0)

---
+++ js/app.js
@@ -0,0 +1,64 @@
+// Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
+
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+
+let team = [
+    {
+        firstName: 'Wayne',
+        lastName: 'Barnet',
+        role: 'Founder & CEO',
+        avatar: `wayne-barnett-founder-ceo.jpg`
+    },
+    {
+        firstName: 'Angela',
+        lastName: 'Caroll',
+        role: 'Chief Editor',
+        avatar: `angela-caroll-chief-editor.jpg`
+    },
+    {
+        firstName: 'Walter',
+        lastName: 'Gordon',
+        role: 'Office Manager',
+        avatar: `walter-gordon-office-manager.jpg`
+    },
+    {
+        firstName: 'Angela',
+        lastName: 'Lopez',
+        role: 'Social Media Manager',
+        avatar: `angela-lopez-social-media-manager.jpg`
+    },
+    {
+        firstName: 'Scott',
+        lastName: 'Estrada',
+        role: 'Developer',
+        avatar: `scott-estrada-developer.jpg`
+    },
+    {
+        firstName: 'Barbara',
+        lastName: 'Ramos',
+        role: 'Founder & CEO',
+        avatar: `barbara-ramos-graphic-designer.jpg`
+    }
+]
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team,
+//  le informazioni di nome, ruolo e la stringa della foto
+//  (in questo fase usiamo il ciclo for…in )
+
+
+
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sotto forma di stringhe
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// Organizzare i singoli membri in card/schede
+//  (trovato un esempio allegato, ma siete liberi di gestire
+//  la grafica a vostro piacimento)
